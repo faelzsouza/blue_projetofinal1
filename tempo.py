5463b650cc1f5173a51fe8e73da427a63f702415
Luiz Fernando Dondici <luizfernando.dondici@gmail.com>
2021-07-06 19:46:12 -0300

    Alteração hora

--- tempo.py
+++ tempo.py
@@ -6,6 +6,9 @@ class Tempo:
         
     def passarHora(self, valor):
         self.hora += valor
+        if self.hora == 24:
+            self.hora = 0
+
         return self.hora
 
     def avancarDia(self):
