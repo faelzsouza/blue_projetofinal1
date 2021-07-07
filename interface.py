8805849247c1ffe89aa9c33dabf1b1abac8a12a6
Rafael <faelzsouza@gmail.com>
2021-07-07 19:43:32 -0300

    correção

--- interface.py
+++ interface.py
@@ -3,11 +3,11 @@
 def introducao():
         print("Bem vindo a Nibiru; Uma corrida pela vida")
 
-        print(f"Olá, que bom que você chegou, realmente precisamos de ajuda! Nibiru já foi um planeta desenvolvido e bem organizado, mas houve um acidente na usina nuclear da província de Namu que lançou material radioativo na atmosfera, desde então a radiação já se espalhou por 70 por cento do planeta, e, segundo as previsões, em até 5 dias essas toxinas chegarão até aqui!\n")
+        print(f"Olá, que bom que você chegou, realmente precisamos de ajuda! Nibiru já foi um planeta desenvolvido e bem organizado, mas houve um acidente na usina nuclear da província de Namu que lançou material radioativo na atmosfera, desde então a radiação já se espalhou 70 por cento do planeta, e, segundo as previsões, em até 5 dias essas toxinas chegarão até aqui!\n")
 
         # E contar o começo da hostoria do personagem...
         #Luca acrônimo de Last Universal Common Ancestor
-        print("Felzmente, nosso herói L.U.C.A, conseguiu fazer os calculos para uma viagem para um planeta azul na Via Láctea. Mas para finalizar o projeto ele precisa de ferramentas para a manutenção de sua nave, além de um estoque de comida para os dias que ficará no espaço! Você está pronto para ajudá-lo nesta jornada interestelar? \U0001F680")
+        print("Felizmente, nosso herói L.U.C.A, conseguiu fazer os cálculos para uma viagem para um planeta azul na Via Láctea. Mas para finalizar o projeto ele precisa de ferramentas para a manutenção de sua nave, além de um estoque de comida para os dias que ficará no espaço! Você está pronto para ajudá-lo nesta jornada interestelar? \U0001F680")
 
 
 
